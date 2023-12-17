46702710c46b897e9b609a9380a9eded7f42e3b7
Felipe Lira <filipi6219@gmail.com>
2023-12-17 11:56:09 -0300

    feat: fazer AddShortUrlController retornar badRequest se a url nao for informada

---
+++ src/presentation/controllers/add-short-url-controller.js
@@ -0,0 +1,20 @@
+import { badRequest, ok, serverError } from '../helpers/http.js';
+
+export default class AddShortUrlController {
+  #generateShortUrlCodeUsecase;
+  #addShortUrlUsecase;
+
+  constructor({ generateShortUrlCodeUsecase, addShortUrlUsecase }) {
+    this.#generateShortUrlCodeUsecase = generateShortUrlCodeUsecase;
+    this.#addShortUrlUsecase = addShortUrlUsecase;
+  }
+
+  async handle({ body: { url, shortCode, validityInDays } }) {
+    try {
+      if (!url) return badRequest(new Error('required fields not informed'));
+    } catch (error) {
+      console.log(error);
+      return serverError();
+    }
+  }
+}
