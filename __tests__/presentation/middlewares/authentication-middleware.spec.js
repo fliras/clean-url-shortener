b2452837e4b9a0ae1d7653e756f1adc7aee4464c
Felipe Lira <filipi6219@gmail.com>
2023-12-30 12:09:54 -0300

    feat: fazer AuthenticationMiddleware chamar CheckUserByIdUsecase com os valores corretos

--- __tests__/presentation/middlewares/authentication-middleware.spec.js
+++ __tests__/presentation/middlewares/authentication-middleware.spec.js
@@ -1,10 +1,32 @@
 import AuthenticationMiddleware from '@/presentation/middlewares/authentication-middleware';
 import { unauthorized } from '@/presentation/helpers/http.js';
 
+class CheckUserByIdUsecaseStub {
+  async handle() {
+    return true;
+  }
+}
+
+const makeSut = () => {
+  const checkUserByIdUsecase = new CheckUserByIdUsecaseStub();
+  const sut = new AuthenticationMiddleware({ checkUserByIdUsecase });
+  return {
+    checkUserByIdUsecase,
+    sut,
+  };
+};
+
 describe('AuthenticationMiddleware', () => {
   it('Should return UnauthorizedError if accessToken is not provided', async () => {
-    const sut = new AuthenticationMiddleware();
+    const { sut } = makeSut();
     const output = await sut.handle({});
     expect(output).toEqual(unauthorized());
+  });
+
+  it('Should call CheckUserByIdUsecase with correct values', async () => {
+    const { checkUserByIdUsecase, sut } = makeSut();
+    const checkUserSpy = jest.spyOn(checkUserByIdUsecase, 'handle');
+    await sut.handle({ accessToken: 'any-token' });
+    expect(checkUserSpy).toHaveBeenCalledWith('any-token');
   });
 });
--- src/presentation/middlewares/authentication-middleware.js
+++ src/presentation/middlewares/authentication-middleware.js
@@ -1,7 +1,14 @@
 import { unauthorized } from '../helpers/http.js';
 
 export default class AuthenticationMiddleware {
+  #checkUserByIdUsecase;
+
+  constructor({ checkUserByIdUsecase }) {
+    this.#checkUserByIdUsecase = checkUserByIdUsecase;
+  }
+
   async handle({ accessToken }) {
     if (!accessToken) return unauthorized();
+    await this.#checkUserByIdUsecase.handle(accessToken);
   }
 }
