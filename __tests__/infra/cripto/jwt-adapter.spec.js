b123ab2fe3caf512a98a5297f226d4e20716b806
Felipe Lira <filipi6219@gmail.com>
2023-12-31 17:48:29 -0300

    test: fazer JwtAdapter lancar excecao quando jwt.verify quebrar

--- __tests__/infra/cripto/jwt-adapter.spec.js
+++ __tests__/infra/cripto/jwt-adapter.spec.js
@@ -16,6 +16,10 @@ const makeSut = () => {
   };
 };
 
+const mockThrow = () => {
+  throw new Error();
+};
+
 describe('JwtAdapter', () => {
   describe('verify()', () => {
     it('Should call jwt.verify with correct values', async () => {
@@ -24,6 +28,13 @@ describe('JwtAdapter', () => {
       const token = 'token';
       await sut.verify(token);
       expect(verifySpy).toHaveBeenCalledWith(token, secret);
+    });
+
+    it('Should throw if jwt.verify throws', async () => {
+      const { sut } = makeSut();
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(mockThrow);
+      const output = sut.verify();
+      expect(output).rejects.toThrow();
     });
   });
 });
