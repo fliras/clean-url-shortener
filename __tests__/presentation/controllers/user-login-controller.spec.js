34f600c5534373ec2a7cacf826399ffd02a5f7e5
Felipe Lira <filipi6219@gmail.com>
2024-01-01 18:01:07 -0300

    test: refatorar testes

--- __tests__/presentation/controllers/user-login-controller.spec.js
+++ __tests__/presentation/controllers/user-login-controller.spec.js
@@ -3,24 +3,37 @@ import MissingParamError from '@/presentation/errors/missing-param-error.js';
 import { badRequest } from '@/presentation/helpers/http.js';
 import { UserLoginUsecaseStub } from '@/tests/presentation/mocks/users.js';
 
+const makeSut = () => {
+  const userLoginUsecase = new UserLoginUsecaseStub();
+  const sut = new UserLoginController({ userLoginUsecase });
+  return {
+    userLoginUsecase,
+    sut,
+  };
+};
+
+const mockRequest = () => ({
+  username: 'any-username',
+  password: 'any-password',
+});
+
 describe('UserLoginController', () => {
   it('Should return badRequest if username is not provided', async () => {
-    const sut = new UserLoginController({});
+    const { sut } = makeSut();
     const output = await sut.handle({ password: 'any-password' });
     expect(output).toEqual(badRequest(new MissingParamError('username')));
   });
 
   it('Should return badRequest if password is not provided', async () => {
-    const sut = new UserLoginController({});
+    const { sut } = makeSut();
     const output = await sut.handle({ username: 'any-username' });
     expect(output).toEqual(badRequest(new MissingParamError('password')));
   });
 
   it('Should call UserLoginUsecase with the correct values', async () => {
-    const userLoginUsecase = new UserLoginUsecaseStub();
-    const sut = new UserLoginController({ userLoginUsecase });
+    const { userLoginUsecase, sut } = makeSut();
     const userLoginSpy = jest.spyOn(userLoginUsecase, 'handle');
-    const request = { username: 'any-username', password: 'any-password' };
+    const request = mockRequest();
     await sut.handle(request);
     expect(userLoginSpy).toHaveBeenCalledWith(request);
   });
