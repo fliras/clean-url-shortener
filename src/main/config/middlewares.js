69785e4ec7d428c8748a58a9edec3ac56655725f
Felipe Lira <filipi6219@gmail.com>
2024-01-01 11:37:35 -0300

    fix: remover middleware duplicado

--- src/main/config/middlewares.js
+++ src/main/config/middlewares.js
@@ -1,7 +1,5 @@
-import authentication from '../middlewares/authentication.js';
 import bodyParser from '../middlewares/body-parser.js';
 
 export default (app) => {
   app.use(bodyParser);
-  app.use(authentication);
 };
